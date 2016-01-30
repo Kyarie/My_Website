7b6d3e6b028d6f94be9bbab9234aa4bbb3907d72
Kyarie <dora-hu@hotmail.com>
2016-01-30 10:58:52 -0500

    Setting up

---
+++ My_Website/app/Home/HomePageController.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('app', []);
+
+app.directive('dynamic', function ($compile) {
+    return {
+        restrict: 'A',
+        replace: true,
+        link: function (scope, ele, attrs) {
+            ele.html(scope.html);
+            $compile(ele.contents())(scope);
+        }
+    };
+});
+
+function MyController($scope) {
+    $scope.click = function (arg) {
+        alert('Clicked ' + $scope.name);
+    }
+    $scope.name = 'whatever';
+    $scope.html = '<a ng-click="click(1)" href="#">Click me</a><label>{{name}}</label><br /> <input type="text" ng-model="name"></input>';
+}
---
+++ My_Website/app/myWebsite.app.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('myWebsite.app', [
+        // Angular modules 
+        'ngAnimate',        // animations
+     //  'ngRoute',          // routing
+        'ui.router',
+        'ngSanitize',       // sanitizes html bindings (ex: sidebar.js)
+         'ngResource',
+         'ngLocalize',
+          'ngLocale',
+     //   //// Custom modules 
+       'common',           // common functions, logger, spinner
+     //   //'common.bootstrap', // bootstrap dialog wrapper functions
+
+     //   // 3rd Party Modules
+        'ui.bootstrap',     // ui-bootstrap (ex: carousel, pagination, dialog)
+     //   //'ngGrid',
+        'angularFileUpload',
+        'smart-table',
+      //  'textAngular',
+        'angularUtils.directives.uiBreadcrumbs',
+        'notificationWidget',
+        'ngCookies',
+        'ui.mask'
+      
+    ]);
+
+    
+
+    // Handle routing errors and success events
+    app.run([ function () {
+        // Include $route to kick start the router.
+    }]);
+})();
