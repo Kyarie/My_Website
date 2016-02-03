1818f499799b6cf45ab78d19102420b6833d4cb0
Kyarie <dora-hu@hotmail.com>
2016-02-02 22:26:16 -0500

    Design adjustment to fit mobile

--- My_Website/app/Common/Menu/menu.js
+++ My_Website/app/Common/Menu/menu.js
@@ -1,13 +1,13 @@
 ﻿var myApp = angular.module('myApp', ['ui.router']);
 
-myApp.run(function () {
-
-});
+myApp.run(['$state', function ($state) {
+    $state.go("homePage");
+}]);
 
 myApp.config(['$stateProvider', function ($stateProvider) {
     $stateProvider
             .state("homePage", {
-                url: "/homePage",
+                url: "/",
                 templateUrl: "../../Home/HomePage.html"
                 //controller: "homePageController",
                 //controllerAs: "homePageController",
@@ -17,26 +17,26 @@ myApp.config(['$stateProvider', function ($stateProvider) {
             })
 }]);
 
-myApp.controller('myAppController', ['$state', '$scope', function ($state, $scope) {
+myApp.controller('myAppController', ['$state', '$scope', '$window', function ($state, $scope, $window) {
     var ctrl = this;
+    var headerHeight = document.getElementById('headerImage').clientWidth;
+    $scope.headerStyle = { height: headerHeight + 'px' }
 
+    $(window).on("resize.doResize", function () {
+        headerHeight = document.getElementById('headerImage').clientWidth;
+        $scope.headerStyle = { height: headerHeight + 'px' }
+        $scope.$apply(function () {
+            //do something to update current scope based on the new innerWidth and let angular update the view.
+        });
+    });
+
+    $scope.$on("$destroy", function () {
+        $(window).off("resize.doResize"); //remove the handler added earlier
+    });     
+    
     $scope.toHome = function () {
         $state.go('homePage');
     };
 
     $scope.testing = 'whatever';
 }]);
-
-//(function () {
-//    var myApp = angular.module('myApp', ['myApp.directive']);
-
-//    myApp.controller('myAppController', ['$state', '$scope', function ($state, $scope) {
-//        var ctrl = this;
-
-//        $scope.toHome = function () {
-//            $state.go('homePage');
-//        };
-
-//        $scope.testing = 'whatever';
-//    }]);
-//})();
