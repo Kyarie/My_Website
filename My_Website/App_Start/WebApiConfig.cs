889b8c1bd51c675177cef96abf08c22a78aae118
Kyarie <dora-hu@hotmail.com>
2017-05-13 22:39:37 -0400

    Spring 2017 Co-op Search Prep

---
+++ My_Website/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace My_Website.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "API/{controller}/{action}/{id}",
+                defaults: new { controller = "CollegeDashboardNew", action = "ReturnSomething", id = RouteParameter.Optional }
+            );
+
+            // Force WebAPI to return JSON
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
+            config.Filters.Add(new EDCSValidationtExceptionFilter());
+        }
+    }
+}
